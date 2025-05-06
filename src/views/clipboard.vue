bcd81c0018be7624d1eac05acf4028cd89556b27
luoyisen <1402175410@qq.com>
2025-05-06 09:34:46 +0800

    test

--- src/views/clipboard.vue
+++ src/views/clipboard.vue
@@ -12,24 +12,26 @@
     <h3>测试用例：所有测试内容应以用户无感为最优体验</h3>
     <h4>1.云机复制到本机</h4>
     <ul>
-      <li>在输入框中输入任意内容，点击按钮，输入框中的内容复制到剪切板，可到任意文本编辑区域粘贴测试</li>
+      <li>
+        在输入框中输入任意内容，点击按钮，输入框中的内容复制到剪切板，可到任意文本编辑区域粘贴测试
+      </li>
     </ul>
     <h4>2.本机复制到云机</h4>
     <ul>
-      <li>
-        在浏览器本标签页中复制文本，alert 复制的文本内容
-      </li>
+      <li>在浏览器本标签页中复制文本，alert 复制的文本内容</li>
       <li>
         在浏览器其他的标签页复制文本，回来浏览器本标签页，alert 复制的文本内容
       </li>
       <li>
-        在非本浏览器的其他应用中复制文本，回来浏览器本标签页，alert 复制的文本内容
+        在非本浏览器的其他应用中复制文本，回来浏览器本标签页，alert
+        复制的文本内容
       </li>
       <li>
         在非本浏览器的其他应用中复制文本，刷新本标签页，alert 复制的文本内容
       </li>
       <li>
-        在非本浏览器的其他应用中复制文本，新建标签页访问本链接，alert 复制的文本内容
+        在非本浏览器的其他应用中复制文本，新建标签页访问本链接，alert
+        复制的文本内容
       </li>
     </ul>
   </div>
@@ -40,16 +42,22 @@ export default {
   data() {
     return {
       text: '123',
+      interval: undefined,
     }
   },
   created() {
     this.getClipboardContents()
     document.addEventListener('copy', this.oncopy)
     document.addEventListener('visibilitychange', this.onvisibilitychange)
+
+    this.interval = setInterval(() => {
+      this.copyToClipboard()
+    }, 3000)
   },
   beforeDestroy() {
     document.removeEventListener('copy', this.oncopy)
     document.removeEventListener('visibilitychange', this.onvisibilitychange)
+    clearInterval(this.interval)
   },
   methods: {
     async onvisibilitychange() {
