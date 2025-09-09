52585b5708bf2a00a718db832b1a5135ed57e591
luoyisen <1402175410@qq.com>
2025-09-09 11:43:03 +0800

    fix

--- src/views/inputfileTest/index.vue
+++ src/views/inputfileTest/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div>从相册选择</div>
-    <input type="file" accept="image/*" />
+    <input type="file" accept="image/*" capture="filesystem" />
     <div>后置摄像头</div>
     <input type="file" accept="image/*" capture="environment" />
     <div>前置摄像头</div>
