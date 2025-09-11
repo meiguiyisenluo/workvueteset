729b8b4fc27a7d7b5127ec33ba79b412bf570fcb
lys <1402175410@qq.com>
2025-09-11 21:26:04 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import { Toast } from 'vant'
-import { encode, decode } from 'url-safe-base64'
+import { decode } from 'url-safe-base64'
 
 // 在升级了Vue-Router版本到到3.1.0及以上之后，页面在跳转路由时控制台会报Uncaught (in promise)的问题
 const originalPush = VueRouter.prototype.push
@@ -120,6 +120,10 @@ const routes = [
     path: '/cropperjsTest',
     component: () => import('@/views/cropperjsTest/Index.vue'),
   },
+  {
+    path: '/urlsafebase64',
+    component: () => import('@/views/urlsafebase64/Index.vue'),
+  },
 ]
 
 const router = new VueRouter({
@@ -127,7 +131,7 @@ const router = new VueRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  // console.log(to.query)
+  console.log(to.query)
   if (to.query.token) {
     removeSSOParam(to.query)
   }
@@ -137,8 +141,10 @@ router.beforeEach((to, from, next) => {
 router.afterEach((to) => {
   const { activityUrl } = to.query
   console.log(activityUrl)
-  const original = atob(decode(activityUrl)) // → 还原原始字符串
-  console.log(original)
+  if (activityUrl) {
+    const original = atob(decode(activityUrl)) // → 还原原始字符串
+    console.log(original)
+  }
 })
 
 // 移除单点登录参数
@@ -151,15 +157,3 @@ function removeSSOParam(param) {
 }
 
 export default router
-
-// https://localhost:8000/#/?channelSrc=02040&loginType=0&token=ssoToken&activityUrl=https%3A%2F%2Fmail.10086.cn%2Fs%3Ffunc%3Dlogin%3AauthTokenPE%26clientId%3D10110%26token%3D%7B%7BssoTk%7D%7D%26targetSourceId%3D001003%26loginSuccessUrl%3Dhttps%3A%2F%2Fhtml5.mail.10086.cn%2Fhtml%2Frestructure%2Findex.html%23%2Fassistant%3FclientId%3D10110%26channelid%3D900529%26token%3D%7B%7BssoTk%7D%7D%26channelSrc%3Dred-cloudphone-003
-// https://localhost:8000/#/?channelSrc=02040&loginType=0&token=ssoToken&activityUrl=https://mail.10086.cn/s?func=login:authTokenPE&clientId=10110&token={{ssoTk}}&targetSourceId=001003&loginSuccessUrl=https://html5.mail.10086.cn/html/restructure/index.html#/assistant?clientId=10110&channelid=900529&token={{ssoTk}}&channelSrc=red-cloudphone-003
-// https://localhost:8000/#/?channelSrc=02040&loginType=0&token=ssoToken&activityUrl=aHR0cHM6Ly9tYWlsLjEwMDg2LmNuL3M_ZnVuYz1sb2dpbjphdXRoVG9rZW5QRSZjbGllbnRJZD0xMDExMCZ0b2tlbj17e3Nzb1RrfX0mdGFyZ2V0U291cmNlSWQ9MDAxMDAzJmxvZ2luU3VjY2Vzc1VybD1odHRwczovL2h0bWw1Lm1haWwuMTAwODYuY24vaHRtbC9yZXN0cnVjdHVyZS9pbmRleC5odG1sIy9hc3Npc3RhbnQ_Y2xpZW50SWQ9MTAxMTAmY2hhbm5lbGlkPTkwMDUyOSZ0b2tlbj17e3Nzb1RrfX0mY2hhbm5lbFNyYz1yZWQtY2xvdWRwaG9uZS0wMDM=
-
-const activityUrl =
-  'https://mail.10086.cn/s?func=login:authTokenPE&clientId=10110&token={{ssoTk}}&targetSourceId=001003&loginSuccessUrl=https://html5.mail.10086.cn/html/restructure/index.html#/assistant?clientId=10110&channelid=900529&token={{ssoTk}}&channelSrc=red-cloudphone-003'
-const safe = encode(btoa(activityUrl)) // → URL-safe Base64
-console.log(safe) // 输出: aHR0cHM6Ly9leGFtcGxlLmNvbT9hPTEmbj0y（已替换 + / =）
-
-const original = atob(decode(safe)) // → 还原原始字符串
-console.log(original) // 输出: https://example.com?a=1&b=2
---
+++ src/views/urlsafebase64/Index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <van-cell title-class="title" title="url">
+      <textarea
+        style="width: 100%; height: 100px; word-break: break-all"
+        v-model="inputStr"
+      />
+    </van-cell>
+    <van-cell title-class="title" title="base64Str">{{ base64Str }}</van-cell>
+    <van-cell title-class="title" title="encodeStr">{{ encodeStr }}</van-cell>
+    <van-cell title-class="title" title="decodeStr">{{ decodeStr }}</van-cell>
+    <van-cell title-class="title" title="originStr">{{ originStr }}</van-cell>
+  </div>
+</template>
+
+<script>
+import { encode, decode } from 'url-safe-base64'
+export default {
+  data() {
+    return {
+      inputStr:
+        'https://mail.10086.cn/s?func=login:authTokenPE&clientId=10110&token={{ssoTk}}&targetSourceId=001003&loginSuccessUrl=https://html5.mail.10086.cn/html/restructure/index.html#/assistant?clientId=10110&channelid=900529&token={{ssoTk}}&channelSrc=red-cloudphone-003',
+    }
+  },
+  computed: {
+    base64Str() {
+      return btoa(this.inputStr)
+    },
+    encodeStr() {
+      return encode(this.base64Str)
+    },
+    decodeStr() {
+      return decode(this.encodeStr)
+    },
+    originStr() {
+      return atob(this.decodeStr)
+    },
+  },
+}
+</script>
+<style lang="less" scoped>
+.title {
+  flex: 0.1;
+}
+</style>
