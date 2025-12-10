97840fc108cac7f284d044eed159d9aa6cb5b473
luoyisen <1402175410@qq.com>
2025-12-10 11:44:50 +0800

    test

--- src/views/heightTest/index.vue
+++ src/views/heightTest/index.vue
@@ -1,6 +1,10 @@
 <template>
-  <div :style="{ height: viewHeight || '100vh' }">
-    {{ viewHeight }}
+  <div class="container">
+    <div class="wrap">
+      <div class="iframe" :style="{ height: viewHeight || '100vh' }">
+        {{ viewHeight }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,9 +30,21 @@ export default {
 }
 </script>
 
-<style scoped>
-div {
+<style lang="less" scoped>
+div.container {
   width: 100%;
-  background-color: blue;
+  min-height: 100vh;
+  background-image: url('https://obs-wx-ysj-xnl-test-001.obs.dualstack.cidc-rp-12.joint.cmecloud.cn/public/user/phone/screenshot/yn_d62e8cej.png');
+  background-repeat: no-repeat;
+  background-position: 0 0;
+  background-size: 100% 100%;
+  .wrap {
+    width: 100%;
+    height: 100vh;
+    .iframe {
+      width: 100%;
+      background-color: blue;
+    }
+  }
 }
 </style>
